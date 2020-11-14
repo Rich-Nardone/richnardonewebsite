e24dc0f5caf336cf091b240e7eb8695355431cea
EC2 Default User <ec2-user@ip-172-31-30-83.ec2.internal>
2020-11-14 00:52:40 +0000

    now i mean business

--- Integration.py
+++ Integration.py
@@ -8,6 +8,7 @@ from os.path import join, dirname
 import flask
 import flask_sqlalchemy
 import flask_socketio
+from dotenv import load_dotenv
 import random
 import json
 import requests
@@ -18,40 +19,43 @@ from game.game import game, scenario
 from game.game_io import progress, prompt_in, send_out, deconstructPlayer
 from game.player import Player
 from sqlalchemy import update
-from dotenv import load_dotenv
 
 game = flask.Flask(__name__)
 
+
 socketio = flask_socketio.SocketIO(game)
 socketio.init_app(game, cors_allowed_origins="*")
-#database init
-DOTENV_PATH = join(dirname(__file__), "sql.env")
-load_dotenv(DOTENV_PATH)
-SQL_USER = os.environ["SQL_USER"]
-SQL_PWD = os.environ["SQL_PASSWORD"]
-DBUSER = os.environ["USER"]
 
-DATABASE_URI = os.environ["DATABASE_URL"]
+dotenv_path = join(dirname(__file__), "sql.env")
+load_dotenv(dotenv_path)
 
-game.config["SQLALCHEMY_DATABASE_URI"] = DATABASE_URI
+database_uri = os.environ["DATABASE_URL"]
+game.config["SQLALCHEMY_DATABASE_URI"] = database_uri
 
-DB = flask_sqlalchemy.SQLAlchemy(game)
-DB.init_app(game)
-DB.app = game
+db = flask_sqlalchemy.SQLAlchemy(game)
+db.init_app(game)
+db.app = game
+
+db.create_all()
+db.session.commit()
+
+import models
+#===================================================================================
+
 #function that marks and saves progress, either inserting a new character into database or updating an existing one.
 def saveProgress():
     FLAG="INSERT"
     USER = userlist[-1]
-    all_character = [character.characterName for character in DB.session.query(models.character).all()]
-    all_userid = [user_id.user_id for user_id in DB.session.query(models.character).all()]
+    all_character = [character.characterName for character in db.session.query(models.character).all()]
+    all_userid = [user_id.user_id for user_id in db.session.query(models.character).all()]
     dict = {}
     for i in range(len(all_character)):
         dict[all_userid[i]] = all_character[i]
         
     USER=userlist[-1]
-    email = DB.session.query(models.username).filter_by(id=USER).first()
+    email = db.session.query(models.username).filter_by(id=USER).first()
     key = email.id
-    characterList = DB.session.query(models.character).filter_by(user_id=key)    
+    characterList = db.session.query(models.character).filter_by(user_id=key)    
     
     player = Player()
     #needs to pick character by user choice
@@ -70,10 +74,10 @@ def saveProgress():
     
     if FLAG == "INSERT":
         chara = models.character(user_id=USER,characterName=statslist[0],str=statslist[1],dex=statslist[2],con=statslist[3],int=statslist[4],cha=statslist[5],luck=statslist[6],max_health=statslist[7],health=statslist[8],max_mana=statslist[9],mana=statslist[10],money=statslist[11], checkpoint=statslist[12])
-        DB.session.add(chara)
-        DB.session.commit()
+        db.session.add(chara)
+        db.session.commit()
     elif FLAG == "UPDATE":
-        chara = DB.session.query(models.character).filter_by(user_id=USER, characterName=statslist[0]).first()
+        chara = db.session.query(models.character).filter_by(user_id=USER, characterName=statslist[0]).first()
         chara.str = statslist[1]
         chara.dex = statslist[2]
         chara.con = statslist[3]
@@ -86,7 +90,7 @@ def saveProgress():
         chara.mana = statslist[10]
         chara.money = statslist[11]
         chara.checkpoint = statslist[12]
-        DB.session.commit()
+        db.session.commit()
     else:
         print("weird error")
 
@@ -102,28 +106,20 @@ def google_login(data):
     userdat = data["UserInfo"]
     profiledat = userdat["profileObj"]
     em=profiledat["email"]
-    all_email = [username.email for username in DB.session.query(models.username).all()]
+    
+    all_email = [username.email for username in db.session.query(models.username).all()]
     if em not in all_email:
         user = models.username(email=em)
-        DB.session.add(user)
-        DB.session.commit()
-    userid = DB.session.query(models.username).filter_by(email=em).first()
+        db.session.add(user)
+        db.session.commit()
+    userid = db.session.query(models.username).filter_by(email=em).first()
     userlist.append(userid.id)
-#Landing page
-@game.route('/')
-def index():
-    return flask.render_template('index.html')
-
-#Route for the main game atm.
-
-@game.route("/main_chat.html")
-def index2():
-    saveProgress()
-    return flask.render_template("main_chat.html")
+    
     
 @socketio.on('user input')
 def parse_user_input(data):
     print(data['input']) #This is what user inputs into the chat command page. Parse data in order to interact with game logic
+    
     
 @socketio.on('user onchat')
 def user_arrived(): 
@@ -133,11 +129,23 @@ def user_arrived():
 @socketio.on('user new character')
 def character_creation(data):
     print(data)
-
+#======================================================================================
+@game.route('/')
+def index(): 
+    return flask.render_template('index.html')
+#======================================================================================
 @game.route('/character_creation.html')
 def char_create(): 
     return flask.render_template('character_creation.html')
     
+#=======================================================================================   
+@game.route('/main_chat.html')
+def main():
+   return flask.render_template('main_chat.html')
+    
+#=======================================================================================
+
+
 # RUNS ON THIS HOST AND PORT
 if __name__ == "__main__":
     socketio.run(
--- models.py
+++ models.py
@@ -4,41 +4,41 @@ from sqlalchemy import Table, Column, Integer, ForeignKey
 from sqlalchemy.orm import relationship
 from sqlalchemy.ext.declarative import declarative_base
 
-from Integration import DB
+from Integration import db
 
-class username(DB.Model):
+class username(db.Model):
     __tablename__ = 'username'
-    id = DB.Column(DB.Integer, primary_key=True)
-    email = DB.Column(DB.String(400))
-    child = DB.relationship("character", backref="userid")
+    id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String(400))
+    child = db.relationship("character", backref="userid")
     
 
-class character(DB.Model):
+class character(db.Model):
     __tablename__ = 'character'
-    id = DB.Column(DB.Integer, primary_key=True)
-    user_id = DB.Column(DB.Integer, DB.ForeignKey('username.id'))
-    characterName = DB.Column(DB.String(400))
-    str = DB.Column(DB.Integer)
-    dex = DB.Column(DB.Integer)
-    con = DB.Column(DB.Integer)
-    int = DB.Column(DB.Integer)
-    cha = DB.Column(DB.Integer)
-    luck = DB.Column(DB.Integer)
-    max_health = DB.Column(DB.Integer)
-    health = DB.Column(DB.Integer)
-    max_mana = DB.Column(DB.Integer)
-    mana = DB.Column(DB.Integer)
-    money = DB.Column(DB.Integer)
-    checkpoint = DB.Column(DB.String(400))
-    gender = DB.Column(DB.String(400))
-    characterClass = DB.Column(DB.String(400))
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('username.id'))
+    characterName = db.Column(db.String(400))
+    str = db.Column(db.Integer)
+    dex = db.Column(db.Integer)
+    con = db.Column(db.Integer)
+    int = db.Column(db.Integer)
+    cha = db.Column(db.Integer)
+    luck = db.Column(db.Integer)
+    max_health = db.Column(db.Integer)
+    health = db.Column(db.Integer)
+    max_mana = db.Column(db.Integer)
+    mana = db.Column(db.Integer)
+    money = db.Column(db.Integer)
+    checkpoint = db.Column(db.String(400))
+    gender = db.Column(db.String(400))
+    characterClass = db.Column(db.String(400))
     
-    child = DB.relationship("inventory", backref="characterid")
+    child = db.relationship("inventory", backref="characterid")
     
     
-class inventory(DB.Model):
+class inventory(db.Model):
     __tablename__ = 'inventory'
-    id = DB.Column(DB.Integer, primary_key=True)
-    character_id = DB.Column(DB.Integer, DB.ForeignKey('character.id'))
-    items = DB.Column(DB.String(400))
+    id = db.Column(db.Integer, primary_key=True)
+    character_id = db.Column(db.Integer, db.ForeignKey('character.id'))
+    items = db.Column(db.String(400))
     
