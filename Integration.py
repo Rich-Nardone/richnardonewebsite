3973068e291e38c36d38ec48ca7f7d20816938db
EC2 Default User <ec2-user@ip-172-31-30-83.ec2.internal>
2020-11-14 02:56:20 +0000

    heroku deployment ready

--- Integration.py
+++ Integration.py
@@ -1,8 +1,8 @@
-#---------------------------------
-#Will integrate database and email
-#---------------------------------
-#Libraries to be used
-#---------------------------------
+# ---------------------------------
+# Will integrate database and email
+# ---------------------------------
+# Libraries to be used
+# ---------------------------------
 import os
 from os.path import join, dirname
 import flask
@@ -13,8 +13,10 @@ from sqlalchemy import update
 import random
 import json
 import requests
+
 # local imports
 import models
+
 # game logic
 import game.game
 import game.game_io
@@ -22,10 +24,10 @@ from game.game import game, scenario
 from game.game_io import progress, prompt_in, send_out, deconstructPlayer
 from game.player import Player
 
-#For shop, checks if item has been purchased.
-item=0
-#Used to check if user bought item again.
-times=1
+# For shop, checks if item has been purchased.
+item = 0
+# Used to check if user bought item again.
+times = 1
 
 game = flask.Flask(__name__)
 
@@ -33,8 +35,8 @@ game = flask.Flask(__name__)
 socketio = flask_socketio.SocketIO(game)
 socketio.init_app(game, cors_allowed_origins="*")
 
-dotenv_path = join(dirname(__file__), "sql.env")
-load_dotenv(dotenv_path)
+#dotenv_path = join(dirname(__file__), "sql.env")
+#load_dotenv(dotenv_path)
 
 database_uri = os.environ["DATABASE_URL"]
 game.config["SQLALCHEMY_DATABASE_URI"] = database_uri
@@ -43,52 +45,78 @@ db = flask_sqlalchemy.SQLAlchemy(game)
 db.init_app(game)
 db.app = game
 
-#===================================================================================
+# ===================================================================================
 
 
-#THESE FUNCTION SEND DUMMY DATA AT THE MOMENT. WILL UPDATE WITH DATABSE INFO EVENTUALLY
-#For shop, checks if item has been purchased.
-item=0
-#Used to check if user bought item again.
-times=1
+# THESE FUNCTION SEND DUMMY DATA AT THE MOMENT. WILL UPDATE WITH DATABSE INFO EVENTUALLY
+# For shop, checks if item has been purchased.
+item = 0
+# Used to check if user bought item again.
+times = 1
 
 
-#function that marks and saves progress, either inserting a new character into database or updating an existing one.
+# function that marks and saves progress, either inserting a new character into database or updating an existing one.
 def saveProgress():
-    FLAG="INSERT"
+    FLAG = "INSERT"
     USER = userlist[-1]
-    all_character = [character.characterName for character in db.session.query(models.character).all()]
-    all_userid = [user_id.user_id for user_id in db.session.query(models.character).all()]
+    all_character = [
+        character.characterName
+        for character in db.session.query(models.character).all()
+    ]
+    all_userid = [
+        user_id.user_id for user_id in db.session.query(models.character).all()
+    ]
     dict = {}
     for i in range(len(all_character)):
         dict[all_userid[i]] = all_character[i]
-        
-    USER=userlist[-1]
+
+    USER = userlist[-1]
     email = db.session.query(models.username).filter_by(id=USER).first()
     key = email.id
-    characterList = db.session.query(models.character).filter_by(user_id=key)    
-    
+    characterList = db.session.query(models.character).filter_by(user_id=key)
+
     player = Player()
-    #needs to pick character by user choice
+    # needs to pick character by user choice
     for char in characterList:
         if char.characterName == "popo":
             player = char
 
     statslist = deconstructPlayer(player)
 
-    for x,y in dict.items():
+    for x, y in dict.items():
         if USER == x and statslist[0] == y:
             FLAG = "UPDATE"
             break
         else:
             FLAG = "INSERT"
-    
+
     if FLAG == "INSERT":
-        chara = models.character(user_id=USER,characterName=statslist[0],str=statslist[1],dex=statslist[2],con=statslist[3],int=statslist[4],cha=statslist[5],luck=statslist[6],max_health=statslist[7],health=statslist[8],max_mana=statslist[9],mana=statslist[10],money=statslist[11], checkpoint=statslist[12], gender=statslist[13],characterClass=statslist[14])
+        chara = models.character(
+            user_id=USER,
+            characterName=statslist[0],
+            str=statslist[1],
+            dex=statslist[2],
+            con=statslist[3],
+            int=statslist[4],
+            cha=statslist[5],
+            luck=statslist[6],
+            max_health=statslist[7],
+            health=statslist[8],
+            max_mana=statslist[9],
+            mana=statslist[10],
+            money=statslist[11],
+            checkpoint=statslist[12],
+            gender=statslist[13],
+            characterClass=statslist[14],
+        )
         db.session.add(chara)
         db.session.commit()
     elif FLAG == "UPDATE":
-        chara = db.session.query(models.character).filter_by(user_id=USER, characterName=statslist[0]).first()
+        chara = (
+            db.session.query(models.character)
+            .filter_by(user_id=USER, characterName=statslist[0])
+            .first()
+        )
         chara.str = statslist[1]
         chara.dex = statslist[2]
         chara.con = statslist[3]
@@ -107,30 +135,42 @@ def saveProgress():
     else:
         print("weird error")
 
+
 def player_info():
-    #player_info = 'lol'
-    player_info = {'user_party': ['player1', 'player2', 'player10'], 'user_inventory': ['coins', 'sword', 'shield'], 'user_chatlog': ['welcome to the world', 'attack', 'user attacks, hitting the blob for 10pts']}
-    if item==1:
-        x=player_info['user_inventory']
+    # player_info = 'lol'
+    player_info = {
+        "user_party": ["player1", "player2", "player10"],
+        "user_inventory": ["coins", "sword", "shield"],
+        "user_chatlog": [
+            "welcome to the world",
+            "attack",
+            "user attacks, hitting the blob for 10pts",
+        ],
+    }
+    if item == 1:
+        x = player_info["user_inventory"]
         global times
-        if times==0:
+        if times == 0:
             x.extend(["Health Pack"])
-            times+=1
+            times += 1
         else:
-            x.extend(["Health Pack"]*times)
-            times+=1
-            
+            x.extend(["Health Pack"] * times)
+            times += 1
+
         print(x)
-        player_info['user_inventory']=x
-    socketio.emit('player info', player_info)
+        player_info["user_inventory"] = x
+    socketio.emit("player info", player_info)
+
 
 userlist = [1]
-@socketio.on('google login')
+
+
+@socketio.on("google login")
 def google_login(data):
     # idinfo contains dictionary of user info
     userdat = data["UserInfo"]
     profiledat = userdat["profileObj"]
-    em=profiledat["email"]
+    em = profiledat["email"]
     all_email = [username.email for username in db.session.query(models.username).all()]
     if em not in all_email:
         user = models.username(email=em)
@@ -138,43 +178,50 @@ def google_login(data):
         db.session.commit()
     userid = db.session.query(models.username).filter_by(email=em).first()
     userlist.append(userid.id)
-    
-@socketio.on('user input')
+
+
+@socketio.on("user input")
 def parse_user_input(data):
-    print(data['input']) #This is what user inputs into the chat command page. Parse data in order to interact with game logic
- 
-@socketio.on('user onchat')
-def user_arrived(): 
-    #THIS IS JUST TEST INPUT THAT IS RECIEVED ON THE FRONTEND SOCKET
+    print(
+        data["input"]
+    )  # This is what user inputs into the chat command page. Parse data in order to interact with game logic
+
+
+@socketio.on("user onchat")
+def user_arrived():
+    # THIS IS JUST TEST INPUT THAT IS RECIEVED ON THE FRONTEND SOCKET
     player_info()
 
-#Test atm for the shop
-@socketio.on('item purchased')
+
+# Test atm for the shop
+@socketio.on("item purchased")
 def item_purchased():
     global item
-    item=1
+    item = 1
     player_info()
-    
-@socketio.on('user new character')
+
+
+@socketio.on("user new character")
 def character_creation(data):
     player = Player()
-    player.id = data['name'] 
-    player.gen = data['gen']
-    player.characterClass = data['classType']
+    player.id = data["name"]
+    player.gen = data["gen"]
+    player.characterClass = data["classType"]
     # data includes character attributes: name, gender and character class
-    if(data['classType']=='Jock'):
+    if data["classType"] == "Jock":
         player.make_jock()
-    elif(data['classType']=='Bookworm'):
+    elif data["classType"] == "Bookworm":
         player.make_bookworm()
-    elif(data['classType']=='NEET'):
+    elif data["classType"] == "NEET":
         player.make_neet()
     USER = userlist[-1]
     email = db.session.query(models.username).filter_by(id=USER).first()
     userid = email.id
-    dbplayer = models.character(user_id=userid,
-        characterClass=data['classType'],
-        characterName=data['name'],
-        gender=data['gen'],
+    dbplayer = models.character(
+        user_id=userid,
+        characterClass=data["classType"],
+        characterName=data["name"],
+        gender=data["gen"],
         str=player.str,
         dex=player.dex,
         con=player.con,
@@ -185,26 +232,32 @@ def character_creation(data):
         health=player.health,
         max_mana=player.max_mana,
         mana=player.mana,
-        money=player.money)
+        money=player.money,
+    )
     db.session.add(dbplayer)
     db.session.commit()
-    
-#======================================================================================
-@game.route('/')
-def index(): 
-    return flask.render_template('index.html')
-  
-#======================================================================================
-@game.route('/character_creation.html')
-def char_create(): 
-    return flask.render_template('character_creation.html')
-#===============================================================   
-@game.route('/main_chat.html')
+
+
+# ======================================================================================
+@game.route("/")
+def index():
+    return flask.render_template("index.html")
+
+
+# ======================================================================================
+@game.route("/character_creation.html")
+def char_create():
+    return flask.render_template("character_creation.html")
+
+
+# ===============================================================
+@game.route("/main_chat.html")
 def main():
-   saveProgress()
-   return flask.render_template('main_chat.html')
-    
-#=======================================================================================
+    saveProgress()
+    return flask.render_template("main_chat.html")
+
+
+# =======================================================================================
 
 # RUNS ON THIS HOST AND PORT
 if __name__ == "__main__":
