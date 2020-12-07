41b2ac17f5ae0cffe110cf3156b613f9be94c331
Patrick R <patrickr2311@gmail.com>
2020-12-07 21:48:34 +0000

    Interface for pulling database data for user

---
+++ user_controller.py
@@ -0,0 +1,37 @@
+import models
+
+class User():
+    def __init__(self, email): 
+        self.email = email
+        self.user_id = None
+        self.selected_character_id = None
+        self.character_counter = 0
+        for x in models.db.session.query(models.username.id).filter(models.username.email == email): 
+            self.user_id = x[0]
+        
+        if self.user_id: 
+            self.char_list = []
+            for z in models.db.session.query(models.character).filter(models.character.user_id == self.user_id): 
+                self.char_list.append(z)
+            self.character_counter = len(self.char_list)
+    
+    def user_exists(self): 
+        if self.user_id: 
+            return True
+        return False
+    
+    def get_characters(self): 
+        display_list = []
+        for x in self.char_list: 
+            info = {}
+            info["id"] = x.id
+            info["character_name"] = x.character_name
+            info["class"] = x.character_class
+            display_list.append(info)
+        return display_list
+    
+    def char_select(self, char_id):
+        self.selected_character_id = char_id
+    
+    def get_inventory(self):
+        pass
