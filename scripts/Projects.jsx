bfaa44e4a133b9fb7487e4d59bcd22b688bd19c3
Rich-Nardone <64598006+Rich-Nardone@users.noreply.github.com>
2022-07-10 21:43:38 -0400

    spelling

--- scripts/Projects.jsx
+++ scripts/Projects.jsx
@@ -48,7 +48,7 @@ export function Projects() {
         <p>Implemented Random Forest Classifier with Python and performed feature engineering to predict Titanic Survival to a 78% accuracy. <a href='https://github.com/Rich-Nardone/Titanic/blob/main/CS675%20Final%20Project.ipynb'>View here</a></p>
         <p>Developed a web scraping tool in Python to pull heart rate monitor data from websites and export specific data to excel sheets for coaches. <a href='https://github.com/Rich-Nardone/ExtractPolarDATA'>View here</a></p>
         <p>Created a Text-RPG game for a capstone project using python flask app and React. <a href='https://github.com/Rich-Nardone/CS490-Text-RPG'>View here</a></p>
-        <p>Developed a poftfolio website using flask and react to display achievements and projects. <a href='https://github.com/Rich-Nardone/richnardonewebsite'>View here</a></p>
+        <p>Developed a portfolio website using flask and react to display achievements and projects. <a href='https://github.com/Rich-Nardone/richnardonewebsite'>View here</a></p>
       </div>
     </div>
   );
